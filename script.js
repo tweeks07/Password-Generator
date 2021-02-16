4799bdca44d1d29c76044deaa3ccd11c746c5942
Taylor Weeks <taylorlweeks707@gmail.com>
2021-02-16 08:10:29 -0500

    second commit

--- script.js
+++ script.js
@@ -21,10 +21,15 @@ generateBtn.addEventListener("click", writePassword);
 //write each in an array
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numbers = ["1","2","3","4","5","6","7","8","9"];
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
 var specialCharacter = ["!","@","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","?","[","]","^","_","'","{","|","}","~"];
 
 //function to get password criteria from user
-//prompt length of password - string
-//confirm characters type - t/f
-//return answers
+
+//User Input
+var characterCnt = prompt("How many characters will your password consist of? Between 8-128");
+
+var confirmNumbers = confrim("Will password contain any numbers?");
+var confirmUpperCase = confirm("Will password contain upper case letters?");
+var confirmLowerCase = confirm("Will password contain lower case letter?");
+var confirmSpecialCharacter = confirm("Will the password contain any special characters?");
